ef67d86c8fe376533c253b229b430c7ac340b6af
CatherineAliceWood <133802370+CatherineAliceWood@users.noreply.github.com>
2023-07-01 18:54:12 +0100

    new pages

---
+++ scripts.js
@@ -0,0 +1,5 @@
+function myFunction(); {
+    let name = prompt('Please enter your name', 'Name');
+    let message = prompt('Please write your message', 'Message');
+document.getElementById(confirmation).innerHTML = 'Thank you for your message!'
+}
