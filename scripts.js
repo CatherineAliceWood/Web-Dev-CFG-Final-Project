2c5adafc491f5c984444e539efa9f759dda7c98f
CatherineAliceWood <133802370+CatherineAliceWood@users.noreply.github.com>
2023-07-04 17:17:45 +0100

    update tuesday 4/7/23

--- scripts.js
+++ scripts.js
@@ -1,5 +1,29 @@
-function myFunction(); {
-    let name = prompt('Please enter your name', 'Name');
-    let message = prompt('Please write your message', 'Message');
-document.getElementById(confirmation).innerHTML = 'Thank you for your message!'
+function llantwitPrompt() {
+  let message = prompt("Did you like Llantwit Major beach?", "yes/no");
+  if (message === "yes") {
+    alert("So did we!");
+  }
+  else {
+    alert("Say what?!");
+  }
+}
+
+function llandudnoPrompt() {
+  let message = prompt("Did you like Llandudno Bay?", "yes/no");
+  if (message === "yes") {
+    alert("Yes! We agree!");
+  }
+  else {
+    alert("Seriously?!");
+  }
+}
+
+function stcatherinesislandPrompt() {
+  let message = prompt("Did you like St. Catherine's Island?", "yes/no");
+  if (message === "yes") {
+    alert("Great taste!");
+  }
+  else {
+    alert("Are you sure?!");
+  }
 }
